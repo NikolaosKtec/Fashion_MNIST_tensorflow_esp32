83a8d4403ffcb17f4182829b86f190a8131a39ad
NIKOLAOS FERREIRA <nikolaosf1daskalakis@gmail.com>
2023-03-12 12:00:26 -0300

    updates

--- teste_adp_micro/read_image_test/read_image_test.ino
+++ teste_adp_micro/read_image_test/read_image_test.ino
@@ -1,39 +1,38 @@
 const byte LED_BUILTIN  = 2; 
-// #include "driver/uart.h"
-#include <typeinfo>
-// // const uart_port_t uart_num = UART_NUM_2;
-// const int BUF_SIZE = 1024;
+#include "mbedtls/base64.h"
 
-// uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
 
-// uart_config_t uart_config = {
-//       .baud_rate = 115200,
-//       .data_bits = UART_DATA_8_BITS,
-//       .parity = UART_PARITY_DISABLE,
-//       .stop_bits = UART_STOP_BITS_1,
-//       .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-//       .rx_flow_ctrl_thresh = 122,
-// };
+unsigned char *input = (unsigned char*)malloc(32); 
 
-uint16_t im;//[28][28][3];
-int x = 0;
-String res = "";
+unsigned char *output = (unsigned char*)malloc(32);
+size_t outlen;
+
+
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  input = (unsigned char*)"abc123"; //reinterpret_cast <unsigned char*>(const_cast<char*>("abc123"));
+  // encode
+  mbedtls_base64_encode(output, 64, &outlen, input, strlen((const char*)input));
 }
  
 void loop() {
  
-   while(Serial.available()){
-    im = Serial.read();
-    delay(10);
-    Serial.write(im);
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
+  Serial.println((char*)output);
+  delay(1000);
+
   
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
+  //  while(Serial.available()){
+  //   im = Serial.read();
+  //   delay(10);
+  //   Serial.write(im);
+  //   digitalWrite(LED_BUILTIN, HIGH);
+  // }
+  
+  // delay(100);
+  // digitalWrite(LED_BUILTIN, LOW);
 }
 
 
