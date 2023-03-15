3c039b5e3a8d05cde2cce2abb895f981e071990f
NIKOLAOS FERREIRA <nikolaosf1daskalakis@gmail.com>
2023-03-15 10:32:10 -0300

    changes

--- teste_adp_micro/read_image_test/read_image_test.ino
+++ teste_adp_micro/read_image_test/read_image_test.ino
@@ -1,10 +1,12 @@
 const byte LED_BUILTIN  = 2; 
 #include "mbedtls/base64.h"
 
+const uint8_t SIZE_BUFF= 32;
+unsigned char *input_for_encode = (unsigned char*)malloc(SIZE_BUFF); 
+unsigned char *input_for_decode = (unsigned char*)malloc(SIZE_BUFF); 
 
-unsigned char *input = (unsigned char*)malloc(32); 
-
-unsigned char *output = (unsigned char*)malloc(32);
+unsigned char *output_encoded = (unsigned char*)malloc(SIZE_BUFF);
+unsigned char *output_decoded = (unsigned char*)malloc(SIZE_BUFF);
 size_t outlen;
 
 
@@ -13,14 +15,20 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  input = (unsigned char*)"abc123"; //reinterpret_cast <unsigned char*>(const_cast<char*>("abc123"));
+  input_for_encode = (unsigned char*)"abc123"; //reinterpret_cast <unsigned char*>(const_cast<char*>("abc123"));
+  input_for_decode = (unsigned char*)"QUEtQkItQ0MtREQtRUUtRkY6MDAwMA==";
   // encode
-  mbedtls_base64_encode(output, 64, &outlen, input, strlen((const char*)input));
+  mbedtls_base64_encode(output_encoded, SIZE_BUFF, &outlen, input_for_encode, strlen((const char*)input_for_encode));
+  // decode
+  mbedtls_base64_decode(output_decoded, SIZE_BUFF, &outlen, input_for_decode, strlen((const char*)input_for_decode));
 }
  
 void loop() {
- 
-  Serial.println((char*)output);
+  Serial.println("encoded");
+  Serial.println((char*)output_encoded);
+
+   Serial.println("decoded");
+  Serial.println((char*)output_decoded);
   delay(1000);
 
   
